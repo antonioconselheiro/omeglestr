a01f4be92a574c36772e44a51d9519e96975756f
António Conselheiro <canudos.sudestino@gmail.com>
2023-11-10 11:16:28 -0300

    organizando estruturas iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'chat',
+    component: ChatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatModule } from './pages/chat/chat.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ChatModule,
     BrowserModule,
     AppRoutingModule
   ],
