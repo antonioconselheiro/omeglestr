27aeefaa54bf76d931c5e56b6d1f639e85658cf6
António Conselheiro <anarcanudos@proton.me>
2024-05-05 15:31:41 -0300

    rascunhando relay para servir como ambiente de desenvolvimento

--- src/app/default-relays.const.ts
+++ src/app/default-relays.const.ts
@@ -1,30 +1,5 @@
 export const defaultRelays = [
-  'wss://nostr.orangepill.dev'
+  'ws://127.0.0.1:7777'
 ];
 
 export const defaultCacheService = [];
-
-//[
-//  'wss://relay.nostr.com.au',
-//  'wss://relay.damus.io',
-//  'wss://nostr-pub.wellorder.net',
-//  'wss://offchain.pub',
-//  'wss://nos.lol',
-//  'wss://relay.snort.social',
-//  'wss://soloco.nl',
-//  'wss://relay.shitforce.one',
-//  'wss://yabu.me',
-//  'wss://nostr.vulpem.com',
-//  'wss://nostrich.friendship.tw',
-//  'wss://n.xmr.se',
-//  'wss://nostr-1.nbo.angani.co',
-//  'wss://eden.nostr.land',
-//  'wss://feeds.nostr.band',
-//  'wss://nostr.wine',
-//  'wss://e.nos.lol',
-//  'wss://nostrue.com',
-//  'wss://nostr.mom',
-//  'wss://nostr.fmt.wiz.biz',
-//  'wss://relay.nostrati.com',
-//  'wss://relay.nostr.com.au'
-//];
